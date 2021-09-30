7f5687473fb24ce64720fdc81bf6d46b181357a7
Lucas Gertz <gertzlucas@gmail.com>
2021-09-30 10:47:36 +0200

    Menu eingefügt und Namen angepasst

--- DBV_230921_V1/Funk.cpp
+++ DBV_230921_V1/Funk.cpp
@@ -4,7 +4,7 @@
 int menu() //Menu-Funktion, gibt Möglichkeiten im Programm auf den Bildschirm aus
 {
 	int abfrage;
-	printf("Menu: \nFolgendes kann getan werden : \n-Datensatz eingeben 1\n-Daten anzeigen 2\n-Datensatz suchen 3\n-Datensatz loeschen oder bearbeiten 4\n-Daten speichern 5\n-Programm benden 0\nIhre Eingabe:");
+	printf("Menu: \nFolgendes kann getan werden : \n-Bild einlesen 1\n-Platzhalter 2\n-Bild anzeigen 3\n-Platzhalter 4\n-Platzhalter 5\n-Programm benden 0\nIhre Eingabe:");
 	scanf("%i", &abfrage);		//wartet auf die Eingabe einer Zahl
 	return(abfrage);		//gibt die eingegebene Zahl an die Main-Funktion zurück
 }
